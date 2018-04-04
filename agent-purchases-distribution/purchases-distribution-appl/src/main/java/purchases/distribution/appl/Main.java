b11e2024a05651764905719e7306eec04cb673bd
Shindarev Nikita <nshindarev@gmail.com>
2018-04-04 21:02:10 +0300

    добавил логгер

--- agent-purchases-distribution/purchases-distribution-appl/src/main/java/purchases/distribution/appl/Main.java
+++ agent-purchases-distribution/purchases-distribution-appl/src/main/java/purchases/distribution/appl/Main.java
@@ -1,7 +1,11 @@
 package purchases.distribution.appl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.print("HW");
+        logger.info("application started");
     }
 }
